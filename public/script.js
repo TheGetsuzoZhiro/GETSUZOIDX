8bceb2fc56246bff054e8a8a68e08e3cd26ff71c
raditexsoo <108138548+TheGetsuzoZhiro@users.noreply.github.com>
2026-07-20 22:14:26 +0700

    Update script.js

--- public/script.js
+++ public/script.js
@@ -4496,34 +4496,22 @@ function updateWinRateChart(data) {
   let winRate = totalClosed > 0 ? (tpCount / totalClosed) * 100 : 0;
   winRate = Math.round(winRate * 10) / 10;
 
-  let winCount = 0,
-    lossCount = 0;
-  let grossProfit = 0,
+  let totalRisk = 0,
+    totalReward = 0,
+    grossProfit = 0,
     grossLoss = 0;
-
   closed.forEach((s) => {
     const ret = s.returnPercent || 0;
     if (ret > 0) {
       grossProfit += ret;
-      winCount++;
-    } else if (ret < 0) {
+      totalReward += ret;
+      totalRisk += ret * 0.5;
+    } else {
       grossLoss += Math.abs(ret);
-      lossCount++;
+      totalRisk += Math.abs(ret);
     }
   });
-
-  let avgRR = "-";
-  if (winCount > 0 && lossCount > 0) {
-    const avgWin = grossProfit / winCount;
-    const avgLoss = grossLoss / lossCount;
-    const ratio = avgWin / avgLoss;
-    avgRR = ratio.toFixed(1);
-  } else if (winCount > 0 && lossCount === 0) {
-    avgRR = "∞";
-  } else {
-    avgRR = "-";
-  }
-
+  const avgRR = totalRisk > 0 ? (totalReward / totalRisk).toFixed(1) : "-";
   const profitFactor =
     grossLoss > 0
       ? (grossProfit / grossLoss).toFixed(2)
